f6d8c30d51435ae987900d479d64677df2ee34e7
Coolp-GitHub <mohanparasinder@gmail.com>
2021-02-28 13:50:59 -0500

    done
    finished, just import

---
+++ size and distance comp/Assets/DistanceCheck.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceCheck : MonoBehaviour
+{
+    public Transform obj1;
+
+    public Transform obj2;
+
+
+    [SerializeField] private Vector3 pos1;
+    [SerializeField] private Vector3 pos2;
+    [SerializeField] private Vector3 posRat;
+
+
+    void Start()
+    {
+        pos1 = new Vector3(obj1.position.x, obj1.position.y, obj1.position.z);
+        pos2 = new Vector3(obj2.position.x, obj2.position.y, obj2.position.z);
+
+    }
+
+
+    void Update()
+    {
+        if (obj1.position.x > obj2.position.x)
+        {
+            posRat = new Vector3(pos2.x / pos1.x, 0, 0);
+        }
+        else if (obj1.position.x < obj2.position.x)
+        {
+            posRat = new Vector3(pos1.x / pos2.x, 0, 0);
+        }
+        else if (obj1.position.x == obj2.position.x)
+        {
+            posRat = new Vector3(1, 1, 1);
+        }
+
+
+
+        //set x, now time to set y
+
+
+
+        if (obj1.position.y > obj2.position.y)
+        {
+            posRat = new Vector3(posRat.x, pos2.y / pos1.y, 0);
+        }
+        else if (obj1.position.y < obj2.position.y)
+        {
+            posRat = new Vector3(posRat.x, pos1.y / pos2.y, 0);
+        }
+        else if (obj1.position.y == obj2.position.y)
+        {
+            posRat = new Vector3(posRat.x, 1);
+        }
+
+
+
+
+
+        if (posRat.x >= 0.5 && posRat.y >= 0.5)
+        {
+            //yourcode
+        }
+        else if (posRat.x >= 0.2 || posRat.y >= 0.2)
+        {
+            //yourcode
+        }
+        else
+        {
+            //yourcode
+        }
+
+
+
+    }
+}
---
+++ size and distance comp/Assets/SizeChecker.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SizeChecker : MonoBehaviour
+{
+    public Transform obj1;
+
+    public Transform obj2;
+
+    
+    [SerializeField] private Vector3 size1;
+    [SerializeField] private Vector3 size2;
+    [SerializeField] private Vector3 sizeRat;
+
+
+    void Start()
+    {
+        size1 = new Vector3(obj1.localScale.x, obj1.localScale.y, obj1.localScale.z);
+        size2 = new Vector3(obj2.localScale.x, obj2.localScale.y, obj2.localScale.z);
+        
+    }
+
+    
+    void Update()
+    {
+        if(obj1.localScale.x > obj2.localScale.x)
+        {
+            sizeRat = new Vector3(size2.x / size1.x,0,0);
+        }
+        else if(obj1.localScale.x < obj2.localScale.x)
+        {
+            sizeRat = new Vector3(size1.x / size2.x,0,0);
+        }else if(obj1.localScale.x == obj2.localScale.x)
+        {
+            sizeRat = new Vector3(1,1,1);
+        }
+
+
+
+        //set x, now time to set y
+
+
+
+        if (obj1.localScale.y > obj2.localScale.y)
+        {
+            sizeRat = new Vector3(sizeRat.x, size2.y / size1.y, 0);
+        }
+        else if (obj1.localScale.y < obj2.localScale.y)
+        {
+            sizeRat = new Vector3(sizeRat.x, size1.y / size2.y, 0);
+        }
+        else if (obj1.localScale.y == obj2.localScale.y)
+        {
+            sizeRat = new Vector3(sizeRat.x, 1, 1);
+        }
+
+        if (sizeRat.x > 0.5 && sizeRat.y >= 0.5)
+        {
+            ////yourcode
+        }
+        else if (sizeRat.x > 0.2 || sizeRat.y >= 0.2)
+        {
+            //yourcode
+        }
+        else
+        {
+            //yourcode
+        }
+
+
+
+    }
+}
